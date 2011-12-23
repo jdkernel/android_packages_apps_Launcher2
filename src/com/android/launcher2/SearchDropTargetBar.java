74e7783ce323619ef2f88b1d83f5a7b9759abad9
jdkoreclipse <jdkoreclipse@gmail.com>
2011-12-23 11:52:19 -0500

    Launcher: speed up rotation animation

--- src/com/android/launcher2/SearchDropTargetBar.java
+++ src/com/android/launcher2/SearchDropTargetBar.java
@@ -159,7 +159,7 @@ public class SearchDropTargetBar extends FrameLayout implements DragController.D
             mQSBSearchBar.setVisibility(View.VISIBLE);
             mQSBSearchBar.setAlpha(1f);
         }
-        mIsSearchBarHidden = false;
+        mIsSearchBarHidden = true;
     }
     public void hideSearchBar(boolean animated) {
         cancelAnimations();
