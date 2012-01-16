4431fb9fdbea806d7068a6159f2cb290b020bc4a
jdkoreclipse <jdkoreclipse@gmail.com>
2012-01-16 09:54:56 -0500

    Revert "Revert "Launcher: Software Menu Button""
    This reverts commit 00733d5186aceacff786db84bb057ed2da91d993.

--- src/com/android/launcher2/Launcher.java
+++ src/com/android/launcher2/Launcher.java
@@ -78,6 +78,7 @@ import android.view.MotionEvent;
 import android.view.Surface;
 import android.view.View;
 import android.view.View.OnLongClickListener;
+import android.view.ViewConfiguration;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.view.WindowManager;
@@ -89,6 +90,7 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Advanceable;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -1769,6 +1771,15 @@ public final class Launcher extends Activity
         }
     }
 
+    public void onClickOverflowMenuButton(View v) {
+        final PopupMenu popupMenu = new PopupMenu(this, v);
+        final Menu menu = popupMenu.getMenu();
+        onCreateOptionsMenu(menu);
+        onPrepareOptionsMenu(menu);
+        popupMenu.show();
+    }
+
+
     void startApplicationDetailsActivity(ComponentName componentName) {
         String packageName = componentName.getPackageName();
         Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
@@ -2778,7 +2789,7 @@ public final class Launcher extends Activity
         // Find the app market activity by resolving an intent.
         // (If multiple app markets are installed, it will return the ResolverActivity.)
         ComponentName activityName = intent.resolveActivity(getPackageManager());
-        if (activityName != null) {
+        if (activityName != null && ViewConfiguration.get(this).hasPermanentMenuKey()) {
             int coi = getCurrentOrientationIndexForGlobalIcons();
             mAppMarketIntent = intent;
             sAppMarketIcon[coi] = updateTextButtonWithIconFromExternalActivity(
@@ -2794,6 +2805,16 @@ public final class Launcher extends Activity
 
     private void updateAppMarketIcon(Drawable.ConstantState d) {
         updateTextButtonWithDrawable(R.id.market_button, d);
+    }
+
+    private void updateOverflowMenuButton() {
+        View overflowMenuButton = findViewById(R.id.overflow_menu_button);
+        if (ViewConfiguration.get(this).hasPermanentMenuKey()) {
+            overflowMenuButton.setVisibility(View.GONE);
+            overflowMenuButton.setEnabled(false);
+        } else {
+            overflowMenuButton.setVisibility(View.VISIBLE);
+        }
     }
 
     /**
@@ -3070,6 +3091,9 @@ public final class Launcher extends Activity
         // Update the market app icon as necessary (the other icons will be managed in response to
         // package changes in bindSearchablesChanged()
         updateAppMarketIcon();
+
+        // Hide overflow menu on devices with a hardkey
+        updateOverflowMenuButton();
 
         mWorkspace.post(mBuildLayersRunnable);
     }
